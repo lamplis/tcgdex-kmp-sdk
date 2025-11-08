ea25844ee9183b9bcaf83e8f564e105cc3d7fb75
lamplis <lamplis@yahoo.fr>
2025-11-08 01:39:54 +0100

    fix(android): prevent crash on Illustrator Collections by hardening preloader and adding Ktor timeouts

--- src/androidMain/kotlin/http/HttpClientFactory.android.kt
+++ src/androidMain/kotlin/http/HttpClientFactory.android.kt
@@ -2,12 +2,33 @@ package app.cardium.tcgdex.sdk.http
 
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.HttpRequestRetry
+import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
+import io.ktor.utils.io.errors.IOException
 
 actual fun createPlatformHttpClient(): HttpClient =
     HttpClient(CIO) {
+        install(HttpTimeout) {
+            requestTimeoutMillis = 12_000
+            connectTimeoutMillis = 8_000
+            socketTimeoutMillis = 12_000
+        }
+
+        install(HttpRequestRetry) {
+            maxRetries = 2
+            retryIf { _, response ->
+                val code = response?.status?.value ?: 0
+                code in 500..599
+            }
+            retryOnExceptionIf { _, cause ->
+                cause is IOException
+            }
+            exponentialDelay()
+        }
+
         install(ContentNegotiation) {
             json(
                 Json {
